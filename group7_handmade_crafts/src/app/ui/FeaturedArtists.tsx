4ec4a8daabb2ac37602c58b5e86b2b540178ff0a
GrandMasterDesigner <elitewebdesign@yahoo.com>
2024-03-16 08:16:59 -0600

    New components created

---
+++ group7_handmade_crafts/src/app/ui/FeaturedArtists.tsx
@@ -0,0 +1,13 @@
+// FeaturedArtists.tsx
+import React from 'react';
+
+const FeaturedArtists: React.FC = () => {
+  return (
+    <div className="featured-artists">
+      <h2>Featured Artists</h2>
+      {/* Add your featured artist components here */}
+    </div>
+  );
+};
+
+export default FeaturedArtists;
---
+++ group7_handmade_crafts/src/app/ui/NewItems.tsx
@@ -0,0 +1,13 @@
+// NewItems.tsx
+import React from 'react';
+
+const NewItems: React.FC = () => {
+  return (
+    <div className="new-items">
+      <h2>New Items</h2>
+      {/* Add your new item components here */}
+    </div>
+  );
+};
+
+export default NewItems;
