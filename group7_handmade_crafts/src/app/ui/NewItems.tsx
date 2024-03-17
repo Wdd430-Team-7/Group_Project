cd5aa2870d2f2d3cc56c8232e88fc7eac9102bda
sunseehray <sabtirazona@byui.edu>
2024-03-17 13:58:25 +0800

    changed image location, did minor tailwindcss for the layout

--- group7_handmade_crafts/src/app/ui/NewItems.tsx
+++ group7_handmade_crafts/src/app/ui/NewItems.tsx
@@ -1,32 +1,35 @@
 // NewItems.tsx
 import Image from "next/image"; // Import next/image
 import React from "react";
+import { patrick_hand } from "./fonts";
 
 const NewItems: React.FC = () => {
   return (
-    <div className="new-items">
+    <div className="new-items flex flex-col">
       <h2>New Items</h2>
-      <div className="item">
-        <Image
-          src="/new_item1.jpg" // Add the path to your image
-          alt="New Item 1"
-          width={200} // Adjust width as needed
-          height={200} // Adjust height as needed
-        />
-        <h3>New Item 1 Name</h3>
-        <p>Description of New Item 1</p>
+      <div className="flex flex-col md:flex-row gap-4 p-4">
+        <div className="item">
+          <Image
+            src="/images/extra-images/new_items/advent-wreath-1069961_1280.jpg" // Add the path to your image
+            alt="New Item 1"
+            width={200} // Adjust width as needed
+            height={200} // Adjust height as needed
+          />
+          <h3>New Item 1 Name</h3>
+          <p>Description of New Item 1</p>
+        </div>
+        <div className="item">
+          <Image
+            src="/images/extra-images/new_items/art-3106207_1280.jpg" // Add the path to your image
+            alt="New Item 2"
+            width={200} // Adjust width as needed
+            height={200} // Adjust height as needed
+          />
+          <h3>New Item 2 Name</h3>
+          <p>Description of New Item 2</p>
+        </div>
+        {/* Add more new items as needed */}
       </div>
-      <div className="item">
-        <Image
-          src="/new_item2.jpg" // Add the path to your image
-          alt="New Item 2"
-          width={200} // Adjust width as needed
-          height={200} // Adjust height as needed
-        />
-        <h3>New Item 2 Name</h3>
-        <p>Description of New Item 2</p>
-      </div>
-      {/* Add more new items as needed */}
     </div>
   );
 };
