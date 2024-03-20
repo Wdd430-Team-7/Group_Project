5915ceb0b72931c6f847327cfc465654fda04300
sunseehray <sabtirazona@byui.edu>
2024-03-20 23:06:32 +0800

    active links done, but the fetch is conflicting with the 'use client'

--- group7_handmade_crafts/src/app/ui/category.tsx
+++ group7_handmade_crafts/src/app/ui/category.tsx
@@ -1,20 +1,29 @@
 import Link from "next/link";
 import { fetchCategories } from "../lib/online-data";
 
-export default async function Category() {
+export default async function Category({ pathname } : { pathname: string}) {
   const categories = await fetchCategories();
+  const isActive = (href: string) => {
+    return pathname === href;
+  };
+
   return (
     <>
       {categories.map((category) => {
         const LinkIcon = category.category_icon;
+        const href = `/category/${category.category_id}`;
         return (
           <Link
             key={category.category_id}
-            href={`/category/${category.category_id}`}
-            className="bg-white px-4 py-2 rounded-md hover:bg-amber-200 text-black"
+            href={href}
+            className={`bg-white px-4 py-2 rounded-md ${
+              isActive(href)
+                ? "bg-green-200 text-black"
+                : "hover:bg-blue-200 text-black"
+            }`}
           >
-          <p className="md:hidden text-2xl">{LinkIcon}</p>
-          <p className="hidden md:block">{category.category_name}</p>
+            <p className="md:hidden text-2xl">{LinkIcon}</p>
+            <p className="hidden md:block">{category.category_name}</p>
           </Link>
         );
       })}
