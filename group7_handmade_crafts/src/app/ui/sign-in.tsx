66cacdd145b1e519c99146cfa4c40132461eb631
sunseehray <sabtirazona@byui.edu>
2024-03-21 09:17:44 +0800

    rebased from main, justify-end sign-in

--- group7_handmade_crafts/src/app/ui/sign-in.tsx
+++ group7_handmade_crafts/src/app/ui/sign-in.tsx
@@ -2,7 +2,7 @@ import { UserCircleIcon } from '@heroicons/react/24/outline';
 
 export default function SignIn(){
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <div className="relative flex flex-1 flex-shrink-0 justify-end">
       <UserCircleIcon className="w-6 text-black" />
     </div>
   );
