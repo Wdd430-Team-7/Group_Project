2488229295f1cfc03a983fe4ad96cf869176bc83
sunseehray <sabtirazona@byui.edu>
2024-03-24 17:00:29 +0800

    commented out console.log so it wont run on vercel's build

--- group7_handmade_crafts/src/app/product/[id]/page.tsx
+++ group7_handmade_crafts/src/app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { fetchAllProducts } from "@/app/lib/online-data";
 export default async function Product(){
   const products = await fetchAllProducts();
-  console.log(products);
+  // console.log(products);
   return <>{products.map((product) => (
     <li key={product.product_id}>{product.product_title} </li>
      
