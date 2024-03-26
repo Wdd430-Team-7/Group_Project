074475a1e637b5bea65854c433e3815a7c3a130c
sunseehray <sabtirazona@byui.edu>
2024-03-26 13:33:43 +0800

    added the select category to the search bar but database interaction not yet ready

--- group7_handmade_crafts/src/app/ui/home-search.tsx
+++ group7_handmade_crafts/src/app/ui/home-search.tsx
@@ -1,9 +1,12 @@
-'use client';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useState } from 'react';
+"use client";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { inter } from "./fonts";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import CategorySelect from "./search/category-select";
+import { Category } from "../lib/definitions";
 
-export default function HomeSearch({ placeholder }: { placeholder: string }){
+export default function HomeSearch({ placeholder, categories }: { placeholder: string, categories: Category[] }) {
   const [query, setQuery] = useState("");
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -12,29 +15,41 @@ export default function HomeSearch({ placeholder }: { placeholder: string }){
 
   const handleSearch = (e: any) => {
     e.preventDefault();
-    if (query.trim() ==='') {
-      router.push('/search');
+    if (query.trim() === "") {
+      router.push("/search");
     } else {
       router.push(`/search?query=${encodeURIComponent(query)}`);
     }
   };
 
   return (
-    <form onSubmit={handleSearch} className="relative flex flex-1 flex-shrink-0">
+    <form
+      onSubmit={handleSearch}
+      className="relative flex flex-1 flex-shrink-0"
+    >
       <label htmlFor="search" className="sr-only text-black">
         Search over thousands of handcrafted artist works
       </label>
       <input
-        type='text'
+        type="text"
         className="peer block w-full rounded-md border border-yellow-500 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 bg-white"
         placeholder={placeholder}
         id="search"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
-      <button type="submit"><MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /></button>
-      
+      <button type="submit">
+        <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      </button>
+      <select
+        id="category_id"
+        name="category_id"
+        className={`${inter.className} absolute right-3 self-center bg-white text-right w-16 md:w-auto`}
+      >
+        <option value="0">Any</option>
+        <CategorySelect categories={categories}/>
+      </select>
     </form>
   );
 }
---
+++ group7_handmade_crafts/src/app/ui/search/category-select.tsx
@@ -0,0 +1,19 @@
+import { Category } from "@/app/lib/definitions";
+import { patrick_hand, inter } from "../fonts";
+
+
+export default function CategorySelect({ categories }: {categories: Category[]}) {  
+    
+  return (
+    <>
+        {categories.map((category) => {
+            const id = category.category_id;
+            const name = category.category_name;
+            const icon = category.category_icon;
+            return (
+                <option key={id} value={id} className={`${inter.className}`}>{name}</option>
+            );
+        })}
+    </>
+  );
+};
