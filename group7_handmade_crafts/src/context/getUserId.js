9f1d23aa7cc32d228f62bcd0238b14ade7f20a8b
mictlantecutli <moisesparra82@hotmail.com>
2024-04-04 01:04:57 -0600

    erase folders of global contect

--- group7_handmade_crafts/src/context/getUserId.js
+++
@@ -1,8 +0,0 @@
-import { auth } from "../../auth";
-import { fetchAccountById, fetchAccountByEmail } from "../app/lib/online-data";
-
-const session = await auth();
-const email = session?.user?.email;
-const account = await fetchAccountByEmail(email);
-
-export const accountId = account?.account_id || "";
